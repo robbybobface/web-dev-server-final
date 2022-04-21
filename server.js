f986b6e87d73915eaaca2d781192f6fe3985d5f8
Jarrett Anderson <jarrett2000@gmail.com>
2022-04-21 17:16:12 -0400

    more testing

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(cors({
 
 app.use(express.json());
 
-const secret = process.env.SECRET;
+const secret = 'thisisthesecretcode';
 
 const store = new MongoDBStore({
         mongoUrl: dbUrl,
